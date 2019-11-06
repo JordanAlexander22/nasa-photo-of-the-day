0cf7d422cac254fd45444ca2cbf9e5fdcdb0a122
jordan <jordan@jordans-Air.lan>
2019-11-06 20:50:16 +0700

    have compenents but fetch from nasa failing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [nasaData, setnasaData]= useState([]);
+
+  const getPics = () => {
+    axios
+      .get(`http://moontrek.jpl.nasa.gov/trektiles/Moon/EQ/LRO_WAC_Mosaic_Global_303ppd_v02/1.0.0/default/default028mm/0/0/0.jpg`)
+      .then(nasaData => {
+        console.log(nasaData);
+        setnasaData(nasaData.data);
+      })
+      .catch(fail => console.log("failed to work"));
+  };
+
+  useEffect(() => {
+    getPics();
+  } , []);
+
   return (
     <div className="App">
+      <h1>Greetings from Outer Space!</h1>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        lets take a look at what we can see outside of this world today!
       </p>
     </div>
   );
